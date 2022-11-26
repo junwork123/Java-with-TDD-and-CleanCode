9f52969de6fd415962488e479d9ec357958ff8f6
Junhyeok Choi <junwork123@gmail.com>
2022-11-26 16:07:24 +0900

    TDD 계산기 기본 기능 구현

--- src/main/java/org/calculator/Calculator.java
+++ src/main/java/org/calculator/Calculator.java
@@ -1,35 +1,61 @@
 package org.calculator;
 
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Calculator {
-    int add(int a, int b) {
+    final Pattern NUMBER_PATTERN = Pattern.compile("[-+][0-9]+");
+    final Pattern OPERATOR_PATTERN = Pattern.compile("[-+/*]");
+
+    private int add(int a, int b) {
         return a + b;
     }
-    int subtract(int a, int b) {
+    private int subtract(int a, int b) {
         return a - b;
     }
-    int multiply(int a, int b) {
+    private int multiply(int a, int b) {
         return a * b;
     }
-    int divide(int a, int b) {
+    private int divide(int a, int b) {
         return a / b;
     }
-    String[] inputString(Scanner sc){
-        String input = sc.nextLine();
-        return input.split(" ");
-    }
-    void parse(String[] split){
-        Pattern patternInt = Pattern.compile("[-|+][0-9]+");
-
-        for(String item : split){
-            Matcher matcher = patternInt.matcher(item);
-            if(matcher.matches()){
-                int parseInt = Integer.parseInt(item);
-            }
+    private boolean isNumber(String item){
+        Matcher matcher = NUMBER_PATTERN.matcher(item);
+        if(matcher.matches()){
+            return true;
         }
+        return false;
+    }
 
+    private boolean isOperator(String item){
+        Matcher matcher = NUMBER_PATTERN.matcher(item);
+        if(matcher.matches()){
+            return true;
+        }
+        return false;
+    }
+
+    Map<String, List<String>> parse(String input){
+        String[] split = input.split(" ");
+        List<String> numbers = Arrays.stream(split).filter(this::isNumber).toList();
+        List<String> ops = Arrays.stream(split).filter(this::isOperator).toList();
+
+        Map<String, List<String>> result = null;
+        result.put("numbers", numbers);
+        result.put("ops", ops);
+        return result;
+    }
+
+    private double calculate(List<String> numbers, List<String> ops){
+        return 0;
+    }
+
+    public double calculate(String input){
+        Map<String, List<String>> map = parse(input);
+        return calculate(map.get("numbers"), map.get("ops"));
     }
 }
--- src/test/java/org/calculator/CalculatorTest.java
+++ src/test/java/org/calculator/CalculatorTest.java
@@ -7,18 +7,27 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.*;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 class CalculatorTest {
-
     Calculator cal;
-
+    Scanner sc;
+    String getTestInput(String input){
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
     @BeforeEach
     public void setUp()  {
         cal = new Calculator();
+
+        // 출력 설정
         OutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
     }
@@ -27,37 +36,43 @@ class CalculatorTest {
     public void tearDown() {
         cal = null;
     }
-
-    @Test
-    public void 덧셈()  {
-        assertEquals(7, cal.add(3, 4));
-    }
-
-    @Test
-    public void 뺄셈()  {
-        assertEquals(1, cal.subtract(5,  4));
-    }
-
-    @Test
-    public void 곱셉()  {
-        assertEquals(6, cal.multiply(2, 3));
-    }
-
-    @Test
-    public void 나눗셈()  {
-        assertEquals(2, cal.divide(8, 4));
-    }
+//
+//    @Test
+//    public void 덧셈()  {
+//        assertEquals(7, cal.add(3, 4));
+//    }
+//
+//    @Test
+//    public void 뺄셈()  {
+//        assertEquals(1, cal.subtract(5,  4));
+//    }
+//
+//    @Test
+//    public void 곱셉()  {
+//        assertEquals(6, cal.multiply(2, 3));
+//    }
+//
+//    @Test
+//    public void 나눗셈()  {
+//        assertEquals(2, cal.divide(8, 4));
+//    }
 
     @ParameterizedTest(name = "문자열 입력 테스트")
     @ValueSource(strings = {"1 + 2", "1 - 2", "1 * 2", "1 / 2"})
-    public void 문자열_입력(String input){
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        Scanner sc = new Scanner(System.in);
-        String[] actual = cal.inputString(sc);
+    public void 문자열_입력_2항(String input){
+        Map<String, List<String>> resultMap = cal.parse(input);
+    }
 
-        assertThat(actual[0]).isEqualTo("1");
-        assertThat(actual[2]).isEqualTo("2");
+    @ParameterizedTest(name = "문자열 입력 테스트 실패")
+    @ValueSource(strings = {"123", "1 2", "1  2", "1 q 2", "1 2 3"})
+    public void 문자열_입력_실패(String input){
+        Map<String, List<String>> resultMap = cal.parse(input);
+    }
+
+    @ParameterizedTest(name = "문자열 계산 테스트")
+    @ValueSource(strings = {"1 + 2", "1 - 2", "1 * 2", "1 / 2"})
+    public void 문자열_계산(String input){
+        double result = cal.calculate(input);
     }
 
 }
