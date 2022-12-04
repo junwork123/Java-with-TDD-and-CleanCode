6f5dd78c015054779083ad0a0c50946bc29d1794
wnpqwnls5544 <junwork123@gmail.com>
2022-12-04 19:02:02 +0900

    클래스 명칭 변경(Ball -> Balls) 정답 생성 로직 유연화
    Signed-off-by: wnpqwnls5544 <junwork123@gmail.com>

--- src/main/java/org/cleancode/baseballgame/object/Balls.java
+++ src/main/java/org/cleancode/baseballgame/object/Balls.java
@@ -5,20 +5,24 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-public class Ball {
+public class Balls {
+    public final int MAX_COUNT = 3;
     private final String answer;
-
+    public Balls(){
+        answer = makeRandomAnswer();
+    }
     public String getAnswer() {
         return answer;
     }
-    public Ball(){
+    private String makeRandomAnswer(){
         // 111~999까지 정수 반환
         // 0이 나오지 않도록 해야함
         // 서로 다른 수가 나와야 함
         List<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
-        int i__ = numbers.remove(new Random().nextInt(numbers.size()));
-        int _i_ = numbers.remove(new Random().nextInt(numbers.size()));
-        int __i = numbers.remove(new Random().nextInt(numbers.size()));
-        answer = String.valueOf((i__ * 100) + (_i_ * 10) + __i);
+        int result = 0;
+        for(int i=MAX_COUNT; i>0; i--){
+            result += (numbers.remove(new Random().nextInt(numbers.size())) * (int) Math.pow(10, i));
+        }
+        return String.valueOf(result);
     }
 }
