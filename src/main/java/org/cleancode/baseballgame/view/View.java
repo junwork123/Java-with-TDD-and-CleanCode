138d7cf060f9773edd92454debf4d9fc522c6b45
wnpqwnls5544 <junwork123@gmail.com>
2022-12-04 17:46:10 +0900

    정답 갯수 출력 변경
    Signed-off-by: wnpqwnls5544 <junwork123@gmail.com>

--- src/main/java/org/cleancode/baseballgame/view/View.java
+++ src/main/java/org/cleancode/baseballgame/view/View.java
@@ -8,8 +8,17 @@ public class View{
             display(Menu.CORRECT_ANSWER);
             return;
         }
-        System.out.printf("%d볼 ", result.getBalls());
-        System.out.printf("%d스트라이크 \n", result.getStrikes());
+        if(result.getBalls() == 0 && result.getStrikes() > 0){
+            System.out.println("4볼");
+            return;
+        }
+        if(result.getBalls() > 0){
+            System.out.printf("%d볼 ", result.getBalls());
+        }
+        if(result.getStrikes() > 0){
+            System.out.printf("%d스트라이크", result.getStrikes());
+        }
+        System.out.println();
     }
     public void display(Menu menu) {
         System.out.println(menu.getMsg());
