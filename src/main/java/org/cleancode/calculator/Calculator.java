7ff8fa391233121a4cbb41c6a571d05ee24d3a97
Junhyeok Choi <junwork123@gmail.com>
2022-12-28 00:46:59 +0900

    계산기 리팩토링 반영 및 테스트 작성
    Signed-off-by: Junhyeok Choi <junwork123@gmail.com>

--- src/main/java/org/cleancode/calculator/Calculator.java
+++ src/main/java/org/cleancode/calculator/Calculator.java
@@ -1,86 +1,31 @@
 package org.cleancode.calculator;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import org.cleancode.calculator.calcuable.Number;
+import org.cleancode.calculator.calcuable.Operator;
+import org.cleancode.calculator.handler.StringParser;
+
 import java.util.List;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
-/**
- * 사용자가 입력한 문자열 값에 따라 사칙연산을 수행할 수 있는 계산기를 구현해야 한다.
- * 문자열 계산기는 사칙연산의 계산 우선순위가 아닌 입력 값에 따라 계산 순서가 결정된다.
- * 즉, 수학에서는 곱셈, 나눗셈이 덧셈, 뺄셈 보다 먼저 계산해야 하지만 이를 무시한다.
- * 예를 들어 "2 + 3 * 4 / 2"와 같은 문자열을 입력할 경우 2 + 3 * 4 / 2 실행 결과인 10을 출력해야 한다.
- */
 public class Calculator {
-    final Pattern NUMBER_PATTERN = Pattern.compile("^[-+]?[0-9]+$");
-    final Pattern OPERATOR_PATTERN = Pattern.compile("^[-+/*]{1}$");
 
-    public int add(int a, int b) {
-        return a + b;
-    }
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-    public int divide(int a, int b) {
-        return a / b;
-    }
-    public boolean isNumber(String item){
-        Matcher matcher = NUMBER_PATTERN.matcher(item);
-        return matcher.matches();
-    }
-
-    public boolean isOperator(String item){
-        Matcher matcher = OPERATOR_PATTERN.matcher(item);
-        return matcher.matches();
-    }
-
-    Map<String, List<String>> parse(String input){
-        String[] split = input.split(" ");
-        List<String> numbers = Arrays.stream(split)
-                .filter(this::isNumber)
-                .collect(Collectors.toList());
-        List<String> ops = Arrays.stream(split)
-                .filter(this::isOperator)
-                .collect(Collectors.toList());
-
-        if (numbers.isEmpty() || ops.isEmpty()){
-            throw new IllegalArgumentException("잘못된 계산식입니다.");
-        }
-
-        Map<String, List<String>> result = new HashMap<>();
-        result.put("numbers", numbers);
-        result.put("ops", ops);
-
-        return result;
-    }
-
-    public int calculate(int x, int y, String op){
-        int result;
+    private Number calculate(Number x, Number y, Operator op){
+        Number result;
         switch (op){
-            case "+": result = add(x,y); break;
-            case "-": result = subtract(x,y); break;
-            case "*": result = multiply(x,y); break;
-            case "/": result = divide(x,y); break;
+            case ADD: result = x.add(y); break;
+            case SUBTRACT: result = x.subtract(y); break;
+            case MULTIPLY: result = x.multiply(y); break;
+            case DIVIDE: result = x.divide(y); break;
             default: throw new IllegalArgumentException("잘못된 연산자입니다.");
         }
         return result;
     }
-    public int calculate(String input){
-        Map<String, List<String>> map = parse(input);
-        List<String> numbers = map.get("numbers");
-        List<String> ops = map.get("ops");
+    public Number calculate(String input){
+        List<Number> numbers = StringParser.parseNumbers(input);
+        List<Operator> ops = StringParser.parseOperators(input);
 
-        int result = 0, x = 0, y = 0;
-        for(String op : ops){
-            x = Integer.parseInt(numbers.remove(0));
-            y = Integer.parseInt(numbers.remove(0));
-            result += calculate(x,y,op);
+        Number result = new Number(0);
+        for(Operator op : ops){
+            result.add(calculate(numbers.remove(0),numbers.remove(0),op));
         }
 
         return result;
--- src/test/java/org/cleancode/calculator/CalculatorTest.java
+++ src/test/java/org/cleancode/calculator/CalculatorTest.java
@@ -1,5 +1,10 @@
 package org.cleancode.calculator;
 
+import org.cleancode.calculator.calcuable.Number;
+import org.cleancode.calculator.calcuable.Operator;
+import org.cleancode.calculator.handler.NumberHandler;
+import org.cleancode.calculator.handler.OperatorHandler;
+import org.cleancode.calculator.handler.StringParser;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,7 +16,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.List;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -35,63 +39,65 @@ class CalculatorTest {
     }
     @Test
     public void 덧셈()  {
-        assertEquals(7, calculator.add(3, 4));
+        assertEquals(new Number(10), new Number(5).add(new Number(5)));
     }
     @Test
     public void 뺄셈()  {
-        assertEquals(1, calculator.subtract(5,  4));
+        assertEquals(new Number(0), new Number(5).subtract(new Number(5)));
     }
     @Test
-    public void 곱셉()  {
-        assertEquals(6, calculator.multiply(2, 3));
+    public void 곱셈()  {
+        assertEquals(new Number(25), new Number(5).multiply(new Number(5)));
     }
     @Test
     public void 나눗셈()  {
-        assertEquals(2, calculator.divide(8, 4));
+        assertEquals(new Number(1), new Number(5).divide(new Number(5)));
     }
     @Test
     public void 숫자_판별(){
-        assertThat(calculator.isNumber("123")).isEqualTo(true);
-        assertThat(calculator.isNumber("+123")).isEqualTo(true);
-        assertThat(calculator.isNumber("1q2w3e4r")).isEqualTo(false);
-        assertThat(calculator.isNumber("--123")).isEqualTo(false);
-        assertThat(calculator.isNumber("-123+")).isEqualTo(false);
-        assertThat(calculator.isNumber("-")).isEqualTo(false);
-        assertThat(calculator.isNumber("-+")).isEqualTo(false);
+        assertThat(NumberHandler.supports("123")).isEqualTo(true);
+        assertThat(NumberHandler.supports("+123")).isEqualTo(true);
+        assertThat(NumberHandler.supports("1q2w3e4r")).isEqualTo(false);
+        assertThat(NumberHandler.supports("--123")).isEqualTo(false);
+        assertThat(NumberHandler.supports("-123+")).isEqualTo(false);
+        assertThat(NumberHandler.supports("-")).isEqualTo(false);
+        assertThat(NumberHandler.supports("-+")).isEqualTo(false);
     }
     @Test
     public void 연산자_판별(){
-        assertThat(calculator.isOperator("+")).isEqualTo(true);
-        assertThat(calculator.isOperator("+-")).isEqualTo(false);
-        assertThat(calculator.isOperator("+-/*")).isEqualTo(false);
-        assertThat(calculator.isOperator("qwe")).isEqualTo(false);
-        assertThat(calculator.isOperator("~")).isEqualTo(false);
+        assertThat(OperatorHandler.supports("+")).isEqualTo(true);
+        assertThat(OperatorHandler.supports("+-")).isEqualTo(false);
+        assertThat(OperatorHandler.supports("+-/*")).isEqualTo(false);
+        assertThat(OperatorHandler.supports("qwe")).isEqualTo(false);
+        assertThat(OperatorHandler.supports("~")).isEqualTo(false);
     }
 
     @ParameterizedTest(name = "문자열 입력 테스트")
     @ValueSource(strings = {"1 + 2", "1 - 2", "1 * 2", "1 / 2"})
     public void 문자열_입력_2항(String input){
-        Map<String, List<String>> resultMap = calculator.parse(input);
+        List<Number> numbers = StringParser.parseNumbers(input);
+        List<Operator> ops = StringParser.parseOperators(input);
 
-        assertThat(resultMap.size()).isEqualTo(2);
-        assertThat(resultMap.get("numbers").size()).isEqualTo(2);
-        assertThat(resultMap.get("numbers").get(0)).isEqualTo("1");
-        assertThat(resultMap.get("numbers").get(1)).isEqualTo("2");
-        assertThat(resultMap.get("ops").size()).isEqualTo(1);
-        assertThat(calculator.isOperator(resultMap.get("ops").get(0))).isEqualTo(true);
+        assertThat(numbers.size()).isEqualTo(2);
+        assertThat(numbers.get(0)).isEqualTo(new Number(1));
+        assertThat(numbers.get(1)).isEqualTo(new Number(2));
+        assertThat(ops.size()).isEqualTo(1);
+        assertThat(ops.get(0)).isEqualTo(Operator.of(input.split(" ")[1]));
     }
 
     @ParameterizedTest(name = "문자열 입력 테스트 실패")
     @ValueSource(strings = {"123", "1 2", "1  2", "1 q 2", "1 2 3"})
     public void 문자열_입력_실패(String input){
-        assertThatThrownBy( () -> calculator.parse(input))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy( () -> {
+            StringParser.parseNumbers(input);
+            StringParser.parseOperators(input);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest(name = "문자열 계산 테스트")
     @CsvSource(value = {"1 + 2:3", "1 - 2:-1", "1 * 2:2", "1 / 2:0"}, delimiter = ':')
-    public void 문자열_계산(String input, int expected){
-        int result = calculator.calculate(input);
+    public void 문자열_계산(String input, Number expected){
+        Number result = calculator.calculate(input);
 
         assertThat(result).isEqualTo(expected);
     }
